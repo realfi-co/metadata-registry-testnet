8b44c47d1f529f41a4c328621bd66f6d2d8ad7a6
Jann Müller <jann.mueller@iohk.io>
2026-02-09 14:55:45 +0800

    Add sUSDr, add logos

--- usdr.sh
+++ usdr.sh
@@ -8,23 +8,39 @@ METADATA_CREATOR=~/token-metadata-creator
 POLICY_ID=45df5f274b8950b512b08d10656864958659c4ecf3ffad092ef63024
 
 # Asset name (hex encoded)
-ASSSET_NAME=55534472
+USDR_ASSSET_NAME=55534472
+SUSDR_ASSET_NAME=7355534472
 
-SUBJECT=$POLICY_ID$ASSET_NAME
-
-$METADATA_CREATOR entry --init $SUBJECT
-
-$METADATA_CREATOR entry $SUBJECT \
-  --name "USDr" \
-  --description "Yield-bearing stablecoin by RealFi Co." \
-  --ticker "USDR" \
-  --decimals 6
+USDR_SUBJECT=$POLICY_ID$USDR_SUBJECT
+SUSDR_SUBJECT=$POLICY_ID$SUSDR_SUBJECT
 
 # TODO:
 # Add signer -- need the skey
 # Add URL
-# Add logo (PNG)
 
-JSON_FILE=$($METADATA_CREATOR entry $SUBJECT --finalize)
 
-mv $JSON_FILE registry/$JSON_FILE
+$METADATA_CREATOR entry --init $USDR_SUBJECT
+
+$METADATA_CREATOR entry $USDR_SUBJECT \
+  --name "USDr" \
+  --description "Yield-bearing stablecoin by RealFi Co." \
+  --ticker "USDR" \
+  --decimals 6 \
+  --logo usdr.png
+
+USDR_JSON_FILE=$($METADATA_CREATOR entry $USDR_SUBJECT --finalize)
+
+mv $USDR_JSON_FILE registry/$USDR_JSON_FILE
+
+$METADATA_CREATOR entry --init $SUSDR_SUBJECT
+
+$METADATA_CREATOR entry $SUSDR_SUBJECT \
+  --name "sUSDr" \
+  --description "staked USDr" \
+  --ticker "SUSDR" \
+  --decimals 6 \
+  --logo susdr.png
+
+SUSDR_JSON_FILE=$($METADATA_CREATOR entry $SUSDR_SUBJECT --finalize)
+
+mv $SUSDR_JSON_FILE registry/$SUSDR_JSON_FILE
